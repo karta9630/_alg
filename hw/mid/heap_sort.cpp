310021b5955d1f3f1bbfb7b69c3dcbe21c4840d4
karta9630 <4909044@gmail.com>
2024-12-11 11:43:23 +0800

    1

--- hw/mid/heap_sort.cpp
+++ hw/mid/heap_sort.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 #include<algorithm>
 using namespace std;
 
@@ -7,49 +8,54 @@ using namespace std;
 //         arr[i]=arr[j];
 //         arr[j]=tmp;
 // }
-//n為樹的節點
-void heap(int tree[], int i,int n){
+//n為樹的節點，i為父節點，要找子節點
+void heap(vector<int>& v, int i,int n){
     if(i>=n) return ;
-    int c1 =2*i+1;
-    int c2 =2*i+2;
-    int max=i;
-    // int parent=(i-1)/2;
-    if (c1<n && tree[max]<tree[c1]){
+    int c1 =2*i+1;//第一子節點公式
+    int c2 =2*i+2;//第二子節點公式
+    int max=i;//設為最大父節點來判斷
+    if (c1<n && v[max]<v[c1]){
         max=c1;
     }
-    if(c2<n && tree[max]<tree[c2]){
+    if(c2<n && v[max]<v[c2]){
         max=c2;
     }
     if(max!=i){
-    swap(tree[max],tree[i]);
-    heap(tree,max,n);
+    swap(v[max],v[i]);
+    heap(v,max,n);
     }
 }
-//下往上
-void bulid_heap(int tree[],int n){
+//max heap由下往上做heap
+void bulid_heap(vector<int>& v,int n){
     int last=n-1;
-    int parent =(last-1)/2;
-    for(int i=parent;i>=0;i--){
-        heap(tree,i,n);
+    int parent =(last-1)/2;//父節點公式
+    for(int i=parent;i>=0;i--){//從最後一個的父節點開始heap
+        heap(v,i,n);
     }
 }
 
-void sort_heap(int tree[],int n){
-    bulid_heap(tree,n);
+void sort_heap(vector<int>& v,int n){
+    bulid_heap(v,n);
     for(int i=n-1;i>=0;i--){
-        swap(tree[0],tree[i]);
-        heap(tree,0,i);
+        swap(v[0],v[i]);
+        heap(v,0,i);
     }
 }
 
 
 
 int main(){
-    int ary[]={2,5,3,1,10,4,6,7};
-    int n=sizeof(ary)/sizeof(ary[0]);
-    sort_heap(ary,n);
-    for (int i=0; i<n;i++){
-        cout<<ary[i]<<" ";
+    vector<int> v={2,3,6,5,4,1,7};
+    vector<int> v1={2,3,6,5,4,1,7};
+    int n=v.size();
+    sort_heap(v1,n);
+    bulid_heap(v,n);
+    for (auto i:v){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+    for (auto i:v1){
+        cout<<i<<" ";
     }
 }
 
--- hw/mid/readme.md
+++ hw/mid/readme.md
@@ -1,2 +1,8 @@
-# 其中作業
-[最大堆排序參考](https://www.youtube.com/watch?v=j-DqQcNPGbE)
+# 期中作業
+heap_sort參考 
+
+https://www.youtube.com/watch?v=j-DqQcNPGbE
+
+https://medium.com/@ralph-tech/%E6%BC%94%E7%AE%97%E6%B3%95%E5%AD%B8%E7%BF%92%E7%AD%86%E8%A8%98-%E5%A0%86%E7%A9%8D%E6%8E%92%E5%BA%8F-heap-sort-4a4f6d48482e
+
+複雜度為nlogn，已經了解運作原理，但是如果再重新實作一次會卡住，如果又回去看參考影片，看著heap_sort運作的流程圖，就可以完全寫出來
